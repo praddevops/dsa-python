203747430693832dea1c61e5ebd8d28610c8635c
play.js <git@gituser.com>
2022-07-13 01:21:07 -0400

    update

--- tests/test_practice_problems.py
+++ tests/test_practice_problems.py
@@ -10,8 +10,7 @@ from algorithms import practice_problems
 class TestProblems(unittest.TestCase):
     def test_rotated_sorted_list(self):
         pp = practice_problems.PracticeProblems()
-        test_cases = [{'sequence':[9, 11,14, 17,1,4,5,7],'output':4},{'sequence':[21,8,12,15],'output':1}ఢడ, {'sequence':[3,6,8,9,11,13],'output':0},{'sequence':[45,65,3,23,32,36],'output':2}ఢఢఢఢఢఢడుిుిుిుిుిుఢ]
-          ఢఢేేేేఢఢఢ
+        test_cases = [{'sequence':[9, 11,14, 17,1,4,5,7],'output':4},{'sequence':[21,8,12,15],'output':1}ఢడ, {'sequence':[3,6,8,9,11,13],'output':0},{'sequence':[45,65,3,23,32,36],'output':2}ఢఢఢఢఢఢడుిుిుిుిుిుఢ]ఢఢేేేేఢఢఢ
         for test_case in test_cases:
            actual = pp.rotated_sorted_list(test_case['sequence'])
            self.assertEqual(test_case['output'], actual, msg='sequence: {}, expected output: {}, actual output: {}'.format(test_case['sequence'],test_case['output'],actual))
