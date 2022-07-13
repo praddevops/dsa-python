bcf0d7fc801f662a838cc630acaac257c5321db9
play.js <git@gituser.com>
2022-07-12 22:57:44 -0400

    update

--- algorithms/practice_problems.py
+++ algorithms/practice_problems.py
@@ -8,11 +8,12 @@ class PracticeProblems():
           while lo < hi:
              mid_position = (lo + hi)//2
              mid_num = sequence[mid_position]
-             if mid_position > 0 and sequence[mid_position-1] < mid_num < sequence[mid_position+1]:
+             if mid_position == 0:
+                return mid_poistion
+             elif mid_position > 0 and sequence[mid_position-1] > mid_num and mid_num < sequence[mid_position+1]:
                 return mid_position
              elif mid_num < sequence[hi]:
                 hi = mid_position - 1
              elif mid_num > sequence[hi]:
                 lo = mid_position + 1
-          return -1          
-          
+          return -1
